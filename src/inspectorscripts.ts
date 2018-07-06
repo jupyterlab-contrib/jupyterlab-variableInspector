dcef8794a948914a143ba24226ccdc1a268049fc
Chapman Siu <chapm0n.siu@gmail.com>
2018-07-06 23:46:05 +1000

    address py2 and py3 and updates content to be inline with Spyder

--- src/inspectorscripts.ts
+++ src/inspectorscripts.ts
@@ -7,56 +7,88 @@ namespace Languages{
     }
 }
 
+
 export
 abstract class Languages{
     /**
      * Init and query script for supported languages.
      */
-   static scripts: { [index: string]: Languages.LanguageModel } = {
-           "python3" : { initScript : `import json\n
-from sys import getsizeof\n
+    static py_script: string = `import json
+from sys import getsizeof
 
-from IPython import get_ipython\n
-from IPython.core.magics.namespace import NamespaceMagics\n
+from IPython import get_ipython
+from IPython.core.magics.namespace import NamespaceMagics
 
-_nms = NamespaceMagics()\n
-_Jupyter = get_ipython()\n
-_nms.shell = _Jupyter.kernel.shell\n
+_nms = NamespaceMagics()
+_Jupyter = get_ipython()
+_nms.shell = _Jupyter.kernel.shell
 
-try:\n
-    import numpy as np  # noqa: F401\n
-except ImportError:\n
-    pass\n
+try:
+    import numpy as np  # noqa: F401
+except ImportError:
+    np = None
 
-
-def _getsizeof(x):\n
-    # return the size of variable x. Amended version of sys.getsizeof\n
-    # which also supports ndarray, Series and DataFrame\n
-    if type(x).__name__ in ['ndarray', 'Series']:\n
-        return x.nbytes\n
-    elif type(x).__name__ == 'DataFrame':\n
-        return x.memory_usage().sum()\n
-    else:\n
-        return getsizeof(x)\n
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
 
 
-def _getshapeof(x):\n
-    # returns the shape of x if it has one\n
-    # returns None otherwise - might want to return an empty string for an empty collum\n
-    try:\n
-        return x.shape\n
-    except AttributeError:  # x does not have a shape\n
-        return None\n
+def _getsizeof(x):
+    # return the size of variable x. Amended version of sys.getsizeof
+    # which also supports ndarray, Series and DataFrame
+    if type(x).__name__ in ['ndarray', 'Series']:
+        return x.nbytes
+    elif type(x).__name__ == 'DataFrame':
+        return x.memory_usage().sum()
+    else:
+        return getsizeof(x)
 
 
-def _var_dic_list():\n
-    types_to_exclude = ['module', 'function', 'builtin_function_or_method','instance', '_Feature', 'type', 'ufunc']\n
-    values = _nms.who_ls()\n
-    vardic = [{'varName': v, 'varType': type(eval(v)).__name__, 'varSize': str(_getsizeof(eval(v))), 'varShape': str(_getshapeof(eval(v))) if _getshapeof(eval(v)) else '', 'varContent': str(eval(v))[:200]}  # noqa\n
-        for v in values if ((str(eval(v))[0] != "<") or (isinstance(eval(v), str)))] #Prevent showing classes, modules etc.\n
-    return json.dumps(vardic)\n
-                        `,
-                        queryCommand : "_var_dic_list()"},
+def _getshapeof(x):
+    # returns content in a friendly way for python variables
+    # pandas and numpy
+    if pd and isinstance(x, pd.DataFrame):
+        return "DataFrame [%d rows x %d cols]" % x.shape
+    if pd and isinstance(x, pd.Series):
+        return "Series [%d rows]" % x.shape
+    if np and isinstance(x, np.ndarray):
+        shape = " x ".join([str(i) for i in x.shape])
+        return "Array [%s]" %  shape
+    return str(x)[:200]
+
+
+def _getcontentof(x):
+    # returns content in a friendly way for python variables
+    # pandas and numpy
+    if pd and isinstance(x, pd.DataFrame):
+        colnames = ', '.join(list(x.columns))
+        return "Column names: %s" % colnames
+    if pd and isinstance(x, pd.Series):
+        return "Series [%d rows]" % x.shape
+    if np and isinstance(x, np.ndarray):
+        return x.__repr__()
+    return str(x)[:200]
+
+
+def _var_dic_list():
+    types_to_exclude = ['module', 'function', 'builtin_function_or_method','instance', '_Feature', 'type', 'ufunc']
+    values = _nms.who_ls()
+    vardic = [{'varName': v, 
+               'varType': type(eval(v)).__name__, 
+               'varSize': str(_getsizeof(eval(v))), 
+               'varShape': str(_getshapeof(eval(v))) if _getshapeof(eval(v)) else '', 
+               'varContent': str(_getcontentof(eval(v)))}  # noqa
+        for v in values if ((str(eval(v))[0] != "<") or (isinstance(eval(v), str)))] #Prevent showing classes, modules etc.
+    return json.dumps(vardic)
+`;
+    static scripts: { [index: string]: Languages.LanguageModel } = {
+           "python3" : { initScript : Languages.py_script,
+    queryCommand : "_var_dic_list()"},
+"python2" : { initScript : Languages.py_script,
+    queryCommand : "_var_dic_list()"},
+"python" : { initScript : Languages.py_script,
+    queryCommand : "_var_dic_list()"}
                 };
    
     public static getScript(lang:string):Promise<Languages.LanguageModel>{
